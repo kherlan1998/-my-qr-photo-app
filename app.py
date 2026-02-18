e977e6d9e402387218979f708745c6d1d77121a6
kherlan1998 <kherlanrojo98@gmail.com>
2026-02-18 18:47:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
 
-app = Flask(__name__)
+# We tell Flask to look for everything in the current directory (.)
+app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# This part is the "secret" page people see after scanning the QR
+@app.route('/view/<int:photo_id>')
+def view_photo(photo_id):
+    return render_template('photo.html', photo_id=photo_id)
+
+# This helper allows Flask to find your images in the main folder
+@app.route('/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('.', filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
